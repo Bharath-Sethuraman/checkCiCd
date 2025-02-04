d82dd14b37a16fee34191181135fbf13f35049cb
Bharath S <sakthivel@bristons.com>
2025-02-04 15:07:04 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import checkFunction from "./controller/check";
 console.log("CHeck123");
-<<<<<<< HEAD
 console.log("Finally Completed3");
 console.log("Finally");
 
-=======
-console.log("Finally Completed4");
->>>>>>> 5ace54b6a77a8718a9e0a692dea1fc74f48f2e8b
 
 
 checkFunction();
